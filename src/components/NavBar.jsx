153c81451bcbbcacdef00c5ea1029e38b9f01889
jose.parada <jose.parada@kitro.ch>
2022-11-26 23:37:45 +0100

    First commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+function NavBar() {
+    return (
+        <div className='flex justify-between items-center w-full h-20 text-black fixed bg-black'>
+            <div>
+                <h1>Holi</h1>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
