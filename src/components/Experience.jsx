48310f40fce1286f4a4812e72c0ed6666ea71efa
jose.parada <jose.parada@kitro.ch>
2022-12-06 17:21:29 +0100

    project done

---
+++ src/components/Experience.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import reactImage from "../assets/react.png";
+import typescript from "../assets/typescript.png";
+import python from "../assets/python.png";
+import aws from "../assets/aws.png";
+import github from "../assets/github.png";
+import gitlab from "../assets/gitlab.png";
+
+const Experience = () => {
+    const techs = [
+        {
+            id: 1,
+            src: reactImage,
+            title: "React",
+            style: "shadow-blue-400",
+        },
+        {
+            id: 2,
+            src: typescript,
+            title: "TypeScript",
+            style: "shadow-blue-600",
+        },
+        {
+            id: 3,
+            src: python,
+            title: "Python",
+            style: "shadow-yellow-500",
+        },
+        {
+            id: 4,
+            src: aws,
+            title: "Amazon Web Services",
+            style: "shadow-gray-600",
+        },
+        {
+            id: 5,
+            src: github,
+            title: "GitHub",
+            style: "shadow-gray-400",
+        },
+        {
+            id: 6,
+            src: gitlab,
+            title: "GitLab",
+            style: "shadow-orange-500",
+        },
+
+    ];
+
+    return (
+        <div
+            name="experience"
+            className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+        >
+            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+                <div>
+                    <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+                        Experience
+                    </p>
+                    <p className="py-6">These are the technologies I've worked with</p>
+                </div>
+
+                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+                    {techs.map(({ id, src, title, style }) => (
+                        <div
+                            key={id}
+                            className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                        >
+                            <img src={src} alt="" className="w-20 mx-auto" />
+                            <p className="mt-4">{title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Experience;
